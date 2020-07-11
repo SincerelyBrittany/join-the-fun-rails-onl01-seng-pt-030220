7d171e3752a6a29f548755d81799e4cfb2912be7
SincerelyBrittany <sincerelybr@gmail.com>
2020-07-11 18:29:38 -0400

    Done.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,2 +1,11 @@
 class Passenger < ActiveRecord::Base
+    has_many :rides
+    has_many :taxis, through: :rides
+#     Taxi -< Rides >- Passengers
+
+# This way, a taxi can have many passengers, and a passenger can have many taxis!
+
+# Each table only has a primary key, so you'll need to write a migration
+# to get the database ready to handle the "has_many :through" relationship.
+
 end
